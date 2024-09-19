8e0409573d453509aba982561faa20102db42fc2
santiago-federici <santiagofederici88@gmail.com>
2024-09-19 07:39:23 -0300

    fix: error not showing the login html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { OAuthModule } from 'angular-oauth2-oidc';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  // imports: [RouterOutlet, LoginComponent, HttpClientModule, OAuthModule.forRoot()],
-  imports: [RouterOutlet, LoginComponent, HttpClientModule, OAuthModule],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(), provideOAuthClient()],
 };
