e6e6ef252efa3d3cff460707c30792ee93e237c9
santiago-federici <santiagofederici88@gmail.com>
2024-09-20 09:31:36 -0300

    update: add header and create home page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,64 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'youtube-data-api';
-  selectedFile: File | null = null;
-
-  userProfile: any;
   constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    if (this.authService.identityClaims) {
-      this.authService.userProfile.subscribe((profile) => {
-        this.userProfile = profile;
-      });
-    }
-  }
-
-  login() {
-    this.authService.login();
-  }
-
-  logout() {
-    this.authService.logout();
-  }
 
   get isLoggedIn() {
     return !!this.authService.identityClaims;
   }
 
-  handleFileInput(event: any) {
-    this.selectedFile = event.target.files[0];
-  }
+  // handleFileInput(event: any) {
+  //   this.selectedFile = event.target.files[0];
+  // }
 
-  async uploadVideo(event: Event) {
-    event.preventDefault();
+  // async uploadVideo(event: Event) {
+  //   event.preventDefault();
 
-    if (!this.selectedFile) {
-      console.error('No video file selected');
-      return;
-    }
+  //   if (!this.selectedFile) {
+  //     console.error('No video file selected');
+  //     return;
+  //   }
 
-    const formData = new FormData();
-    formData.append('video', this.selectedFile);
-    formData.append('access_token', this.authService.accessToken);
+  //   const formData = new FormData();
+  //   formData.append('video', this.selectedFile);
+  //   formData.append('access_token', this.authService.accessToken);
 
-    const res = await fetch('http://localhost:1234/upload-video', {
-      method: 'POST',
-      body: formData,
-    });
+  //   const res = await fetch('http://localhost:1234/upload-video', {
+  //     method: 'POST',
+  //     body: formData,
+  //   });
 
-    const data = await res.json();
-    console.log(data);
-  }
+  //   const data = await res.json();
+  //   console.log(data);
+  // }
 }
