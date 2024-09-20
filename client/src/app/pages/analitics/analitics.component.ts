db01feb470f054283425bf6c560e67c95d276ed4
santiago-federici <santiagofederici88@gmail.com>
2024-09-20 10:03:56 -0300

    update: implement authorization component

--- client/src/app/pages/analitics/analitics.component.ts
+++ client/src/app/pages/analitics/analitics.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RequireAuthorizationComponent } from '../../components/require-authorization/require-authorization.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-analitics',
   standalone: true,
-  imports: [],
+  imports: [RequireAuthorizationComponent],
   templateUrl: './analitics.component.html',
-  styles: ``
+  styles: ``,
 })
 export class AnaliticsComponent {
+  constructor(private authService: AuthService) {}
 
+  get isLoggedIn() {
+    return !!this.authService.identityClaims;
+  }
 }
