db26c3597b573940467862edaecbefa1fcba72a3
santiago-federici <santiagofederici88@gmail.com>
2024-09-20 10:02:03 -0300

    feat: create component

---
+++ client/src/app/components/require-authorization/require-authorization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-require-authorization',
+  standalone: true,
+  imports: [],
+  templateUrl: './require-authorization.component.html',
+})
+export class RequireAuthorizationComponent {}
