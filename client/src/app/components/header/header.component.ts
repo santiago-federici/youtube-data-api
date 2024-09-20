bd29e7ee2440e596f19b531d5ac607c47dfefe94
santiago-federici <santiagofederici88@gmail.com>
2024-09-20 09:31:20 -0300

    feat: create header component

---
+++ client/src/app/components/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  isProfileMenuOpen = false;
+
+  userProfile: any;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    if (this.authService.identityClaims) {
+      this.authService.userProfile.subscribe((profile) => {
+        this.userProfile = profile;
+      });
+    }
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  get isLoggedIn() {
+    return !!this.authService.identityClaims;
+  }
+}
