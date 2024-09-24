b8844c0ec3933ea7873068092f65ae6e353bcab5
santiago-federici <santiagofederici88@gmail.com>
2024-09-24 11:46:05 -0300

    feat: create channel interface

---
+++ client/src/app/models/channel.ts
@@ -0,0 +1,62 @@
+export interface ChannelListResponse {
+  kind?: string;
+  etag?: string;
+  pageInfo?: PageInfo;
+  items?: ChannelItem[];
+}
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface ChannelItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: ChannelSnippet;
+  contentDetails: ChannelContentDetails;
+  statistics: ChannelStatistics;
+}
+
+export interface ChannelSnippet {
+  title: string;
+  description: string;
+  customUrl: string;
+  publishedAt: string;
+  thumbnails: Thumbnails;
+  localized: Localized;
+}
+
+export interface Thumbnails {
+  default: Thumbnail;
+  medium: Thumbnail;
+  high: Thumbnail;
+}
+
+export interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Localized {
+  title: string;
+  description: string;
+}
+
+export interface ChannelContentDetails {
+  relatedPlaylists: RelatedPlaylists;
+}
+
+export interface RelatedPlaylists {
+  likes: string;
+  uploads: string;
+}
+
+export interface ChannelStatistics {
+  hiddenSubscriberCount: boolean;
+  subscriberCount: string;
+  videoCount: string;
+  viewCount: string;
+}
